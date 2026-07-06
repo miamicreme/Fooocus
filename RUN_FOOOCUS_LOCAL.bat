30369b990fc1ddda4b5e8facb6fc47654576da56
Miami Creme Labs <miamicreme@gmail.com>
2026-07-06 00:36:26 -0400

    Make local runner prefer venv Python

--- RUN_FOOOCUS_LOCAL.bat
+++ RUN_FOOOCUS_LOCAL.bat
@@ -1,9 +1,14 @@
 @echo off
 title Fooocus Local Runner
 cd /d %~dp0
+
+set PYTHON_CMD=python
+if exist ".venv\Scripts\python.exe" set PYTHON_CMD=.venv\Scripts\python.exe
+
 echo Starting Fooocus locally...
+echo Using: %PYTHON_CMD%
 echo.
-python launch.py --disable-analytics
+%PYTHON_CMD% launch.py --disable-analytics
 echo.
 echo Fooocus stopped. Press any key to close.
 pause >nul
