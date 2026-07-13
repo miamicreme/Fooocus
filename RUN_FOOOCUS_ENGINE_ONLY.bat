8d389b00fe330001d879edfb2a838a5044a59b90
Miami Creme Labs <miamicreme@gmail.com>
2026-07-13 00:14:04 -0400

    chore: restore direct Fooocus engine launcher

--- RUN_FOOOCUS_ENGINE_ONLY.bat
+++ RUN_FOOOCUS_ENGINE_ONLY.bat
@@ -8,10 +8,10 @@ set GRADIO_VERSION_CHECK=False
 
 if not exist "%TEMP%\fooocus" mkdir "%TEMP%\fooocus"
 
-echo Starting Fooocus engine only with keepalive.
-echo This keeps the process open after Gradio starts at http://127.0.0.1:7865.
+echo Starting Fooocus engine only.
+echo Local URL: http://127.0.0.1:7865
 echo.
-%PYTHON_CMD% scripts\run_fooocus_keepalive.py --disable-analytics --disable-in-browser
+%PYTHON_CMD% launch.py --disable-analytics --disable-in-browser
 
 echo.
 echo Fooocus engine stopped. Press any key to close.
